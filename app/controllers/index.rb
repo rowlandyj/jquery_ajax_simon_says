0bcbb80cbf6f4cf9215389ed150b64ec556da325
Apprentice <apprentice@dbc09.local>
2013-07-02 21:04:47 -0700

    added json objects

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,9 +4,10 @@ get '/' do
 end
 
 post '/color' do
-
+  content_type :json
   #Create and return a JSON object with the random cell and color given below.
 
-  cell= rand(1..9)
+  cell= rand(0..8)
   color= "#" + "%06x" % (rand * 0xffffff)
+  {cell: cell, color: color}.to_json
 end
