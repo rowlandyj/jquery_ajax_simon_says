8c22d4c466f552ae7322ebd1df51075366593c9a
Apprentice <apprentice@dbc09.local>
2013-07-02 21:26:02 -0700

    done

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+  $('#get_color').on('click', function(){
+    $.ajax({
+      url: '/color',
+      method: 'post'
+    }).done(function(response){
+      $('#color_me').children().eq(response.cell).css('background-color', response.color);      
+    });
+  });
 });
